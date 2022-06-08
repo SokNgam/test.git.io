78e7a7c400f6ec73674e1428781ac20bd32c0807
Sokngam <githubkh55@gmail.com>
2022-06-09 06:25:10 +0700

    test

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+$("#btn-login").click(function(){
+    $(".main-bg-form-login").show(200);
+});
+
+$("#close-login").click(function(){
+    $(".main-bg-form-login").hide(200);
+});
+
+$("#btn-register").click(function(){
+    $(".main-bg-form-register").show(200);
+});
+
+$("#close-register").click(function(){
+    $(".main-bg-form-register").hide(200);
+});
+
+$("#btn-login").click(function(){
+    $(".main-bg-form-register").hide(200);
+});
+
+$("#btn-register").click(function(){
+    $(".main-bg-form-login").hide(200);
+});
+
+// Mobile menu
+$("#show-mobile-menu").click(function(){
+    $(".sidenav").show(200);
+});
+
+$(".close-mobile-menu").click(function(){
+    $(".sidenav").hide(200);
+});
+
+$("#user-mobile-menu").click(function(){
+    $(".main-user-for-mobile").show(200);
+});
+
+$("#close-user-login-mobile").click(function(){
+    $(".main-user-for-mobile").hide(200);
+});
+
+$("#user-register-link").click(function(){
+    $(".main-user-register-for-mobile").show(200);
+});
+
+$("#user-register-link").click(function(){
+    $(".main-user-for-mobile").hide(200);
+});
+
+$("#close-user-signup-mobile").click(function(){
+    $(".main-user-register-for-mobile").hide(200);
+});
+
+$("#user-login-link").click(function(){
+    $(".main-user-for-mobile").show(200);
+});
+
+$("#user-login-link").click(function(){
+    $(".main-user-register-for-mobile").hide(200);
+});
