cdec71da71ee32aa9a5ea3d6390b664c8e537685
SokNgam <106458746+SokNgam@users.noreply.github.com>
2022-06-11 06:19:11 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
 $("#btn-login").click(function(){
-    $(".main-bg-form-login").show(200);
+    $(".main-bg-form-login").slideDown(10);
 });
 
 $("#close-login").click(function(){
-    $(".main-bg-form-login").hide(200);
+    $(".main-bg-form-login").slideUp(10);
 });
 
 $("#btn-register").click(function(){
-    $(".main-bg-form-register").show(200);
+    $(".main-bg-form-register").slideDown(10);
 });
 
 $("#close-register").click(function(){
-    $(".main-bg-form-register").hide(200);
+    $(".main-bg-form-register").slideUp(10);
 });
 
 $("#btn-login").click(function(){
-    $(".main-bg-form-register").hide(200);
+    $(".main-bg-form-register").hide(10);
 });
 
 $("#btn-register").click(function(){
-    $(".main-bg-form-login").hide(200);
+    $(".main-bg-form-login").hide(10);
 });
 
 // Mobile menu
@@ -32,29 +32,29 @@ $(".close-mobile-menu").click(function(){
 });
 
 $("#user-mobile-menu").click(function(){
-    $(".main-user-for-mobile").show(200);
+    $(".main-user-for-mobile").show(10);
 });
 
 $("#close-user-login-mobile").click(function(){
-    $(".main-user-for-mobile").hide(200);
+    $(".main-user-for-mobile").hide(10);
 });
 
 $("#user-register-link").click(function(){
-    $(".main-user-register-for-mobile").show(200);
+    $(".main-user-register-for-mobile").show(10);
 });
 
 $("#user-register-link").click(function(){
-    $(".main-user-for-mobile").hide(200);
+    $(".main-user-for-mobile").hide(10);
 });
 
 $("#close-user-signup-mobile").click(function(){
-    $(".main-user-register-for-mobile").hide(200);
+    $(".main-user-register-for-mobile").hide(10);
 });
 
 $("#user-login-link").click(function(){
-    $(".main-user-for-mobile").show(200);
+    $(".main-user-for-mobile").show(10);
 });
 
 $("#user-login-link").click(function(){
-    $(".main-user-register-for-mobile").hide(200);
+    $(".main-user-register-for-mobile").hide(10);
 });
